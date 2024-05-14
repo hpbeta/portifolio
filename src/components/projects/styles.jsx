97a232dbc49de9585ebcdef1cbfe317653ca49ea
João Lucas <hpbeta16.l@gmail.com>
2024-05-13 22:58:47 -0300

    melhoria responsivo

--- src/components/projects/styles.jsx
+++ src/components/projects/styles.jsx
@@ -2,10 +2,10 @@ import { styled } from "styled-components"
 
 export const Container = styled.div`
   text-align: center;
-  margin: 0 auto ;
-  width: 300px;
+  margin: 100px auto ;
+  
 h2{
-  margin-bottom: 20px;
+  margin-bottom: 80px;
   font-size: 30px;
 }
 `
@@ -14,7 +14,7 @@ export const Image = styled.img`
   width: 500px;
   height: 400px;
   @media(max-width: 414px){
-     width: 100%;
+     width: 90%;
      height: 240px;
     }
 `
